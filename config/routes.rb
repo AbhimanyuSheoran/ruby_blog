6c61ebe77ef0e41967d24534e7dd8cd863cc5f73
AbhimanyuSheoran <abhimanyu.sheoran.bits@gmail.com>
2013-04-29 01:44:10 +0530

    adding comments in views

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Blog::Application.routes.draw do
   resources :articles do
   resources :comments
 end
-#match '*path' => redirect('/')
+match '*path' => redirect('/')
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
